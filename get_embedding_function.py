96e12840a5dee17bf4a413d570b468bac0001ff3
Raju1998060745 <99238367+Raju1998060745@users.noreply.github.com>
2024-09-25 10:01:13 -0700

    rag application

---
+++ get_embedding_function.py
@@ -0,0 +1,10 @@
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+
+
+def get_embedding_function():
+    embeddings = OllamaEmbeddings(
+    model="llama3",
+)
+    # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    return embeddings
